c4eebb83900e22a56094c68758377a599d1adc9f
JubsGS <julia.g.serra@unesp.br>
2024-07-30 11:07:26 -0300

    amem, copiei seu codigo

--- App1-Jubs/Calculadora/js/index.js
+++ App1-Jubs/Calculadora/js/index.js
@@ -1,31 +1,61 @@
-let numero = 0;
-        let ultimaOp;
-        let memoria = 0;
-        let resultado = 0;
-        function insert(num){
-            if(num == "+" || num == "-" || num == "*" || num == "/"){
-                
-            }
-            numero = document.getElementById('resultado').innerHTML;
-            document.getElementById('resultado').innerHTML = numero + num;
-        }
-        function allClean(){
-            memoria = 0;
-            document.getElementById('resultado').innerHTML = "";
-        }
-        function clean(){
-            resultado = document.getElementById('resultado').innerHTML;
-            document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-        }
+let operacao = " "
+let ultimaOP = " "
 
-        function equal(){
-            resultado = document.getElementById('resultado').innerHTML;
-            if(resultado){
-                document.getElementById('resultado').innerHTML = eval(resultado);
-                memoria = resultado;
+
+function insert(num){
+    
+    if(ultimaOP == "+" || ultimaOP == "-" || ultimaOP == "*" || ultimaOP == "/"){
+        allClean()       
+    }
+    let numero = document.getElementById("resultado").innerHTML
+    if(numero == "ERROR"){
+        operacao = " "
+        numero = numero.substring(5)
+    }
+    if(numero == "NaN"){
+        operacao = " "
+        numero = numero.substring(3)
+    }
+    
+    document.getElementById("resultado").innerHTML = numero+num
+    if(numero.length > 13){
+        document.getElementById("resultado").innerHTML = numero.substring(0, resultado.length = 13)
+    }
+    ultimaOP = num
+    operacao = operacao + num
+    console.log(operacao)
+}
+
+function clean(){
+    let resultado = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
+    operacao = document.getElementById("resultado").innerHTML
+}
+
+function allClean(bt){
+    document.getElementById("resultado").innerHTML = ""
+    if(bt == 1){
+        operacao = " "
+    }   
+}
+
+function equal(){
+    let res = document.getElementById("resultado")
+    let resultado = res.innerHTML
+    if(resultado){
+        try {
+            res.innerHTML = eval(operacao)
+            resultado = res.innerHTML
+            if(resultado.length > 14){
+                document.getElementById("resultado").innerHTML = resultado.substring(0, 10)
+                console.log(resultado.length)   
             }
-            else{
-                resultado = document.getElementById('resultado').innerHTML = "";
-            }
-            
+        } catch (ERROR) {
+            res.innerHTML = "ERROR"
         }
+        
+        
+        let numero = document.getElementById("resultado").innerHTML
+        operacao = numero
+    }  
+}   
